56561e6c4dd3af808f0cfa7cdd88bb49deddcd7a
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2023-02-04 19:16:53 +0700

    chore production: change the backend base url

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const BACKEND_BASE_URL = 'http://localhost:3000/api';
+export const BACKEND_BASE_URL = 'https://if-reviewed-be.vercel.app/api';
 export const COM_STYLES = [
 	{ value: 1, text: 'Amiable' },
 	{ value: 2, text: 'Driving' },
